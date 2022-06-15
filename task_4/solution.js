a0a832297513aea337f318d69503d578c568d359
laola81 <laola81@yandex.ru>
2022-06-15 13:31:49 +0300

    task_4

--- task_4/solution.js
+++ task_4/solution.js
@@ -7,6 +7,8 @@ function parseProducts(json) {
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
     // Верните как результат функции свойство products объекта data
+    data = JSON.parse(json);
+    return data.products;
 }
 
 // Напишите функцию renderProductsCards(json)
@@ -16,3 +18,11 @@ function parseProducts(json) {
 // Запишите в переменную length значение свойства products.length
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json) {
+    clearProducts;
+    products = parseProducts(json);
+    length = products.length;
+    for (let i = 0; i < products.length; i++) {
+        addProduct(products[i]);
+    }
+}
