7e7c7a48c2dfaff6409a3e8aa88a208711d31a7d
laola81 <laola81@yandex.ru>
2022-06-22 23:26:41 +0300

    task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,21 +33,27 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+function sendRequest(name, phone, {street, house, entrance, floor, flat}, goods, sum) {
+    let data = {
+        client: name + ' ' + phone,
+        order: {
+            address: 'ул. ' + street + ', дом ' + house + ', ' + entrance + ' подъезд, ' + floor + ' этаж, кв ' + flat,
+            sum: sum
+        },
+        goods: []
+    };
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count})
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    //data.order.address = address;
+    //data.order.sum = name + phone + address + goods + sum;
 
-    data.client = 'Иван';
+    //data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
-
+    let jsonData = JSON.stringify({data});
     return jsonData;
 }
